18eab7dd860729ee1faa8bfcd756a5e0b924bc62
Jonathan Avila <jonathan.avila@nyu.edu>
2019-12-01 20:57:45 -0500

    FINISHED PROJECt

--- css_extractor.py
+++ css_extractor.py
@@ -2,53 +2,50 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import requests
 from time import sleep
 from sys import argv
 from os import path
 import re
+import subprocess
 if __name__ == '__main__':
-	url = argv[1]
-	url = url.replace("\n", "").replace("\r", "")
+	url = argv[1]   # loads website from command line arguments
+	url = url.replace("\n", "").replace("\r", "")  # removes whitespace characters
 	chrome_options = Options()
-	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--headless")  # makes the Chrome browser headless
 	driver = webdriver.Chrome(options=chrome_options)
 	driver.get(f"http://{url}/")
-	sleep(5)
-	html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+	sleep(5)  # allows time for selenium to render the Javascript on the sites
+	html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")  # returns rendered site
 	outfile = open("full_site.html", "w+")
 	outfile.write(html)
 	outfile.close()
 	outfile_path = path.abspath("full_site.html")
-	driver.get('file://' + outfile_path)
+	driver.get('file://' + outfile_path)  # Opens loaded site locally
 	sleep(5)
-	elems = driver.find_elements_by_xpath("//link[@href]")
-	css_links = []
-	font_list = []
-	font_file = open("input.txt", "w+")
-	font_file.write(url + "\n")
+	elems = driver.find_elements_by_xpath("//link[@href]")  # acquires all embedded css links found within html site
+	css_hyperlinks = []
+	font_file = open("input.txt", "w+") 
+	font_file.write(url + "\n")  # writes out url into txt file
 	font_file.close()
-	for elem in elems:
-		if (("css" in elem.get_attribute("href")) and (elem.get_attribute("href") not in css_links)):
-			css_links.append(elem.get_attribute("href"))
-	#print(css_links)
-	for elem in css_links:
-		#print(elem)
-		driver.get(elem)
-		sleep(5)
+	for css_lk in elems:
+		if (("css" in css_lk.get_attribute("href")) and (css_lk.get_attribute("href") not in css_hyperlinks)):  # opens all css sites found in html site and makes sure its not already saved in the list
+			css_hyperlinks.append(css_lk.get_attribute("href"))
+	for elem in css_hyperlinks:
 		lst_of_fonts = []
-		css = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML") #Remove this and replace it with wget command
-		result = re.compile('font-family\s*?:\s*?(.*?)\s*?[;\}]')
-		result = re.findall(result, css)
+		try:
+			css = requests.get(elem)  # opens css link to acquire fonts from
+		except:
+			print(elem + ": hyperlink is not a valid http link")
+		css_file = css.text
+		result = re.compile('font-family\s*?:\s*?(.*?)\s*?[;\}]')  # uses a regular expression function to extract the required font data
+		result = re.findall(result, css_file)
 		if result:
-			#print(result)
-			print("\n" + css + "\n")
-			for element in result:
-				element = element.replace("font-family", "").replace("}", "").replace(";", "").replace("{", "").replace(":", "")
-				element = element.split(",")
-				for font in element:
-					#print(font)
-					font_file = open("input.txt", "a")
+			for lst in result:
+				lst = lst.replace("font-family", "").replace("}", "").replace(";", "").replace("{", "").replace(":", "")
+				lst = lst.split(",")
+				for font in lst:
+					font_file = open("input.txt", "a")  # adds font data to txt file
 					font_file.write(font + ",")
-				#print(font + "\n")
 	font_file.close()
 	driver.close()
--- font2csv.py
+++ font2csv.py
@@ -1,61 +1,39 @@
 #!/usr/bin/env python3
+
 import sys
 import urllib.request
 
 if __name__ == '__main__':
 
-	infile = open("output.csv", "r")
+	infile = open("output.csv", "r")  # Opens csv source file
 	if infile.mode == 'r':
-		URL = infile.readline().replace('\n', "").replace('\r',"").replace(" ", "")
+		URL = infile.readline().replace('\n', "").replace('\r',"").replace(" ", "")  #  Removes any whitespace and newline characters
 		URL = URL.replace("\n", "")
-		#(URL + "\n")
-		contents = infile.readline()
-		#print(contents)
+		contents = infile.readline() # Reads in data from source file
 	infile.close()
-	'''
-	num_of_args = len(sys.argv)
-	i = 1
-	string = ""
-
-	while(i < num_of_args):
-			string = string + sys.argv[i]
-			i = i + 1
-
-
-
-	delimeters = ["\t", "\n", "\r"]
-	for dl in delimeters:
-		string = string.replace(dl,"")
-
-
-	list = string.split(",")
-	'''
-	contents = contents.replace('\"', " ").replace('\'', " ")
+	contents = contents.replace('\"', " ").replace('\'', " ")  # removes all single and double quotes from fonts found
 	lst = contents.split(",")
 	lst.pop()
 	already_used =[]
 	outfile = open("fonts.csv", "a+")
 	outfile.write("\r")
 	for element in lst:
-		#(element)
 		if ("w0" in element.lower()):
 			element = element.lower().split("w0")[0]
 		for char in element:
-			if char.isnumeric() and char == element[-1]:
+			if char.isnumeric() and char == element[-1]:  # removes gratuitous versions of fonts found
 				element = element[:-2]
-				#element = element.replace(char, "")
-				#element = element.replace("-", "")
-		element = element.strip()
-		element = element.lower()
-		undesireables = ["arial", "arial black", "avant garde", "bookman", "candara", "century schoolbook", "comic sans ms", "courier", "courier new", "garamond", "georgia", "helvetica", "helvetica neue", "impact", "palatino", "roboto", "tahoma", "times", "times new roman", "trebuchet ms", "verdana", "serif", "sans-serif", "sans"]
-		if (element not in already_used) and (element not in undesireables) and (element.isspace() == False) :
+		element = element.strip()  # removes whitespace and newline characters
+		element = element.lower()  # lowercases all the results
+		undesireables = ["arial", "arial black", "avant garde", "bookman", "candara", "century schoolbook", "comic sans ms", "courier", "courier new", "garamond", "georgia", "helvetica", "helvetica neue", "impact", "palatino", "roboto", "tahoma", "times", "times new roman", "trebuchet ms", "verdana", "serif", "sans-serif", "sans", "monospace", "simple-social-icons"]
+		if (element not in already_used) and (element not in undesireables) and (element.isspace() == False) :  # discards any fonts that are undesireable
 			fonturl = (f"http://fonts.adobe.com/fonts/{element}")
 			try:
-				if urllib.request.urlopen(fonturl).getcode() == 200:
+				if urllib.request.urlopen(fonturl).getcode() == 200:   # adds adobe font along with adobe font url after checking that the adobe font url is functional
 					outfile.write(URL + "," + element + "," + fonturl + "\n")
 					print("entry: "+ URL + "," + element + "," + fonturl)
 			except:
-				outfile.write(URL + "," + element + "\n")
+				outfile.write(URL + "," + element + "\n")  #adds non-adobe font if adobe url does not exist
 				print("entry: "+ URL + "," + element)
 			already_used.append(element)
 	outfile.close()
